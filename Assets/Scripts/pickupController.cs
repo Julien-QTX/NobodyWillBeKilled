85c5856db3148dd76b779c38269c459518caff31
GoncalvesLionel <lionel78330@gmail.com>
2024-11-14 13:45:15 +0100

    cléf

--- Assets/Scripts/pickupController.cs
+++ Assets/Scripts/pickupController.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class pickupController : MonoBehaviour
 {
+   
+
     // Start is called before the first frame update
     void Start()
     {
         
+
+
     }
 
     // Update is called once per frame
@@ -15,8 +20,18 @@ public class pickupController : MonoBehaviour
     {
     }
 
-    void OnCollisionEnter2D(Collision collision)
+    void OnCollisionEnter2D(UnityEngine.Collision2D collision)
     {
-        Destroy(gameObject);
+        if (collision.gameObject.CompareTag("User"))
+        {
+            GameObject[] closedDoors = GameObject.FindGameObjectsWithTag("CloosedDoor");
+            Debug.Log(GameObject.FindGameObjectsWithTag("CloosedDoor"));
+            foreach (GameObject door in closedDoors)
+            {
+                door.tag = "OpenDoor";
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
