e9b9414877ad2c8e11f1cf5e6a66089c9cb9a5c3
coding <coding@MacBook-Pro-de-coding.local>
2024-11-14 13:10:52 +0100

    fin branch

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("User")){
+            rb.WakeUp();
+        }
+    }
+}
